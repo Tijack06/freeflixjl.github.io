48a67d39e05700354ef261b1513ce803c73de35a
Tijack06 <107306275+Tijack06@users.noreply.github.com>
2022-06-26 18:59:37 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,41 @@
+alert("[ Pour les téléphones ]    Si vous-voyez seulement 2 film de large. veuiller dézoomer");
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  var swiper = new Swiper(".swiper-solide box",{
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 55000,
+      disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      568: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      968: {
+        slidesPerView: 5,
+      },
+    },
+  });
+
+
